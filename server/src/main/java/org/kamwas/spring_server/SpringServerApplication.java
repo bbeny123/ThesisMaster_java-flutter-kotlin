59dace26868e307b585bc597b8b9c4796e90f123
Kamil Wasilewski <bbeny123@gmail.com>
2019-06-29 00:12:48 +0200

    reorganize

---
+++ server/src/main/java/org/kamwas/spring_server/SpringServerApplication.java
@@ -0,0 +1,13 @@
+package org.kamwas.spring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerApplication.class, args);
+    }
+
+}
