06ca5a8a0c8ab0f3122802ff4a57f4cc66cff43a
Kamil Wasilewski <bbeny123@gmail.com>
2019-07-01 02:01:52 +0200

    spring rests

--- server/src/main/java/org/kamwas/spring_server/RESTController.java
+++ server/src/main/java/org/kamwas/spring_server/RESTController.java
@@ -1,8 +1,11 @@
 package org.kamwas.spring_server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -10,13 +13,17 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class RESTController {
 
-    @GetMapping("/get")
+    private Logger logger = LoggerFactory.getLogger(RESTController.class);
+
+    @GetMapping("/")
     public ResponseEntity<?> get() {
-        return ok().build();
+        logger.info("GET called");
+        return ok(new User(1L, "user", "user@user", "user", 30));
     }
 
-    @PostMapping("/post")
-    public ResponseEntity<?> post() {
+    @PostMapping("/")
+    public ResponseEntity<?> post(@RequestBody User user) {
+        logger.info("POST called - " + user.toString());
         return ok().build();
     }
 
---
+++ server/src/main/java/org/kamwas/spring_server/User.java
@@ -0,0 +1,72 @@
+package org.kamwas.spring_server;
+
+public class User {
+
+    private Long id;
+    private String login;
+    private String email;
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(Long id, String login, String email, String name, Integer age) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
