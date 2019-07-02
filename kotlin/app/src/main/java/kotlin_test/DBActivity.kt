6e1adb946ecf2daeb72914c184395e1be2e9dc75
Kamil Wasilewski <bbeny123@gmail.com>
2019-07-03 01:05:09 +0200

    migrate to androidx

--- kotlin/app/src/main/java/kotlin_test/DBActivity.kt
+++ kotlin/app/src/main/java/kotlin_test/DBActivity.kt
@@ -1,7 +1,7 @@
 package kotlin_test
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import kotlinx.android.synthetic.main.activity_collection.toolbar
 import kotlinx.android.synthetic.main.activity_db.*
