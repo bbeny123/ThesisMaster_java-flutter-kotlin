52e6be71aaf2d49bcb13c5210de488fc86546016
Kamil Wasilewski <bbeny123@gmail.com>
2019-07-03 03:05:20 +0200

    kotlin fix

--- kotlin/app/src/main/java/kotlin_test/FileActivity.kt
+++ kotlin/app/src/main/java/kotlin_test/FileActivity.kt
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_collection.toolbar
 import kotlinx.android.synthetic.main.activity_file.*
 import java.io.File
-import java.io.RandomAccessFile
+import java.io.FileOutputStream
+import java.security.SecureRandom
 
 class FileActivity : AppCompatActivity() {
 
@@ -33,8 +34,11 @@ actual class FileHelper(private val path: File) {
     actual fun deleteFile(name: String): Boolean = File(path, name).delete()
 
     actual fun generateFile(name: String, size: Int) {
-        val file = RandomAccessFile(File(path, name), "rw")
-        file.setLength((1024 * 1024 * size).toLong())
+        val file = FileOutputStream(File(path, name))
+        val data = ByteArray(1024 * 1024 * size)
+        SecureRandom().nextBytes(data)
+        file.write(data)
+        file.close()
     }
 
 }
