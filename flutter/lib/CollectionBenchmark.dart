a738693db1c9b9cdd4f454ae3b8985f24ffe2a0d
Kamil Wasilewski <bbeny123@gmail.com>
2019-07-03 17:51:36 +0200

    optimize imports

--- flutter/lib/CollectionBenchmark.dart
+++ flutter/lib/CollectionBenchmark.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+
 import 'helper/TestObject.dart';
 
 class CollectionBenchmark {
--- flutter/lib/DeSerBenchmark.dart
+++ flutter/lib/DeSerBenchmark.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'helper/User.dart';
 
 class DeSerBenchmark {
